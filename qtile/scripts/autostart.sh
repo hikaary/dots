2ee025ac711e2594d95e6534a355918550a7c3d2
hikary <tronin371@gmail.com>
2024-01-09 13:54:06 +0300

    Fix lockscreen

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -10,7 +10,5 @@ obsidian &
 gsettings set org.gnome.desktop.interface cursor-theme "Bibata-Modern-Ice" &
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
 swayidle -w \
-            timeout 600 'swaylock --screenshots --clock --indicator
-  --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color 103019 --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --fade-in 0.2' \
-      timeout 1000 'systemctl suspend' \
+      timeout 1000 'systemctl suspend'  &
 
