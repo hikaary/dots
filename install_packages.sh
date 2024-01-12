9fca973060954e3de77d756e4a159c3e1946bb53
hikary <tronin371@gmail.com>
2024-01-13 00:49:42 +0300

    add packages

---
+++ install_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Установка yay..."
+
+# Установка yay
+rm -rf yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
+
+echo "Установка пакетов из pacman_packages.txt..."
+
+sudo pacman -S --noconfirm - < pacman_packages.txt
+
+echo "Установка пакетов из yay_packages.txt..."
+
+yay -S --noconfirm - < yay_packages.txt
